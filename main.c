457b1bd392761b0e12be7b52ba354e3e29452ec0
Jor Bashllari <altnate2000@gmail.com>
2018-11-23 12:56:03 +0100

    cleanup

--- main.c
+++ main.c
@@ -15,6 +15,5 @@ int main(int argc, char const **argv)
     map_t *m = set_map(argv[1]);
     sq_t *sq = mksq(0, 0, 0);
     print_map((char const **) fill_sq(m, find_sq(m, sq, 1)));
-    /* printf("END___SQUARE\tx: %2u\ty: %2u\tlen: %2u\n", sq->x, sq->y, sq->side); */
     return 0;
 }
